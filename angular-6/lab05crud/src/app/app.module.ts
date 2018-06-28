80624a7a05a87736d595eb8775760d0cdbfd5829
Mohammad Rajaul Islam <springapidev@gmail.com>
2018-06-28 10:15:18 +0600

    crud app 1

--- angular-6/lab05crud/src/app/app.module.ts
+++ angular-6/lab05crud/src/app/app.module.ts
@@ -3,10 +3,18 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomeComponent } from './home/home.component';
+import { AboutComponent } from './about/about.component';
+import { UserListComponent } from './user-list/user-list.component';
+import { UserCreateComponent } from './user-create/user-create.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    AboutComponent,
+    UserListComponent,
+    UserCreateComponent
   ],
   imports: [
     BrowserModule,
